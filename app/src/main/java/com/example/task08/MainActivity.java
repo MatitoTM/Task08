d132a7ff7e52084a1d167153e557a3643517216e
MatitoTM <Taitomatan2010@gmail.com>
2026-01-19 09:25:10 +0200

    The structure of the program

--- app/src/main/java/com/example/task08/MainActivity.java
+++ app/src/main/java/com/example/task08/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.task08;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +13,18 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView iV;
+    ImageButton iB;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        iV = findViewById(R.id.iV);
+        iB = findViewById(R.id.iB);
+    }
+
+    public void clicked(View view) {
     }
 }
